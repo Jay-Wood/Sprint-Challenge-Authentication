acc6a74b295d8d3e02a02c518dd2a3ba6799cb1c
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-20 08:43:12 -0700

    Login working without auth middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,39 @@
 const router = require('express').Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const secrets = require("../secrets.js");
+const Users = require("../users/userModel.js");
+const validateUser = require("./authenticate-middleware.js");
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  let hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  const token = generateToken(user);
+  
+  Users.add(user)
+    .then(id => {
+      res.status(201).json({ message: `User registered: ${user.username} has an id of ${id.id}`})  
+    })
+    .catch(error => {
+      res.status(500).json( {message: `Error adding new user: ${error}`} );
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  }
+
+  const options = {
+    expiresIn: "1d"
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
---
+++ users/userModel.js
@@ -0,0 +1,32 @@
+const db = require("../database/dbConfig.js");
+
+module.exports = {
+    getUsers, 
+    getById,
+    getByName,
+    add
+}
+
+function getUsers() {
+    return db("users")
+}
+
+function getById(id) {
+    return db("users")
+        .where("id", id)
+        .first();
+}
+
+function getByName(filter) {
+    return db("users")
+        .where(filter)
+        .first()
+}
+
+function add(user) {
+    return db("users")
+        .insert(user, "id")
+        .then(id => {
+            return (getById(id[0]))
+        })
+}
